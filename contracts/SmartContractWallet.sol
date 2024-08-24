25813be953d226f992ee78f476401b416d1f7a83
Ayush-Kotlin-Dev <shweetarai@gmail.com>
2024-08-25 00:19:31 +0530

    Completed Smart Contract Wallet with Wallet balance recovery method

--- contracts/SmartContractWallet.sol
+++ contracts/SmartContractWallet.sol
@@ -1,15 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+contract Consumer{
 
+    function getBalance() public view returns (uint){
+        return address(this).balance ;
+    }
+
+    function deposit() public payable {}
+}
 contract SmartContractWallet{
 
-    address payable owner ;
+    address payable public owner ;
 
     mapping (address => uint256) public allowance ;
     mapping (address => bool) private _approved ;
+
+    mapping (address=>bool) public guardians ;
+    address payable nextOwner ;
+    mapping (address=> mapping (address => bool)) nextOwnerGuardianVoteBool ;
+
+    uint guardianResetCount ;
+    uint public constant confirmationsFromguardiansForReset = 3 ;
+
     constructor(){
         owner = payable(msg.sender);
+    }
+
+    function setGuardian(address _guardian , bool _isGuardian)public  {
+        require(msg.sender==owner , "NotOwner");
+        guardians[_guardian] = _isGuardian;
+    }
+    function proposeNewOwner(address payable _newOwner) public {
+        require(guardians[msg.sender], "NotGuardian of the wallet , aborting");
+        require(nextOwnerGuardianVoteBool[_newOwner][msg.sender] == false ,"Vote has already been cast");
+
+        if(_newOwner != nextOwner){
+            nextOwner = _newOwner;
+            guardianResetCount = 0;
+        }
+        guardianResetCount ++ ;
+
+        if(guardianResetCount >= confirmationsFromguardiansForReset){
+            owner = nextOwner;
+            nextOwner = payable(address(0));
+        }
+
     }
 
     function setAllowance(address _for , uint _amount) public {
