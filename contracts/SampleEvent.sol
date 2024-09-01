1fe50ddbcbd8fb312f24d0deb5e8e4b2f7237d0f
Ayush-Kotlin-Dev <shweetarai@gmail.com>
2024-09-02 00:07:51 +0530

    Example of Events and return as variables

--- contracts/SampleEvent.sol
+++ contracts/SampleEvent.sol
@@ -20,4 +20,30 @@ contract EventExample {
         return true ;
     }
 
+    event LogEvent(address indexed sender, uint amount);
+
+    function sendEther() public payable {
+        emit LogEvent(msg.sender, msg.value);
+    }
+
+}
+
+
+contract Bank {
+    mapping(address => uint) public balances;
+
+    event Deposit(address indexed account, uint amount);
+    event Withdraw(address indexed account, uint amount);
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value); // Using event as return
+    }
+
+    function withdraw(uint amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Withdraw(msg.sender, amount); // Using event as return
+    }
 }
